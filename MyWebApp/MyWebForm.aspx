88b7b22eb097cf44007cfec639255b6dfd3cf44f
Lavinia <gherasim.lavinia92@gmail.com>
2013-11-27 12:01:05 +0200

    web forms ex

---
+++ MyWebApp/MyWebForm.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyWebForm.aspx.cs" Inherits="MyWebApp.MyWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title id="pageTitle" runat="server"></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Panel runat="server" BorderColor="black" BorderStyle="Dashed" ID="myPanel">
+        <asp:Label id="label" runat="server">Nume</asp:Label>
+        <asp:TextBox runat="server" ID="field"></asp:TextBox>
+        <asp:Button runat="server" Text="Click" onClick="changeText"/>
+        <asp:Button runat="server" Text="New title" onClick="changeTitle"/>
+        <asp:Button runat="server" Text="Remove" onClick="removeButton"/>
+        <asp:Button runat="server" Text="ShowListItem" onClick="showItem"/>
+        <asp:DropDownList runat="server" ID="list">
+            <asp:ListItem>C</asp:ListItem>
+            <asp:ListItem>C++</asp:ListItem>
+            <asp:ListItem>Java</asp:ListItem>
+            <asp:ListItem>.Net</asp:ListItem>
+        </asp:DropDownList>
+    </asp:Panel>
+          <asp:CheckBox runat="server" OnCheckedChanged="modifyPanelState"  AutoPostBack="True" />
+      
+    </div>
+        <asp:Label id="label2" runat="server"></asp:Label>
+        <asp:Button runat="server" Text="Concat" onClick="changeText"/>
+    </form>
+</body>
+</html>
---
+++ MyWebApp/MyWebForm.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApp
+{
+    public partial class MyWebForm : System.Web.UI.Page
+    {
+        public string Title
+        {
+
+            get { return ViewState["titlu"] == null ? "" : ViewState["titlu"].ToString(); }
+            set { ViewState["titlu"] = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            label2.Text += "Page_Load/ ";
+            Button b = new Button();
+            b.Text = "DinamicButton";
+            b.ID = "dinamicButton";
+            b.Click += setLabelText;
+            myPanel.Controls.Add(b);
+
+        }
+
+        protected void Page_Prerender(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Title))
+            {
+                pageTitle.Text = Title;
+            }
+
+            label2.Text += "  Page_Prerender/ ";
+
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+
+            label2.Text += " Page_Init/ ";
+
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+
+            label2.Text += " Page_PreInit/ ";
+
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+
+            label2.Text += " Page_Unload/ ";
+
+        }
+
+
+        public void setLabelText(object sender, EventArgs e)
+        {
+            label.Text = "Dinamic button clicked";
+        }
+
+        public void changeText(object sender, EventArgs e)
+        {
+            string text = Request.Form["field"];
+            field.Text = "";
+            label.Text = text;
+        }
+
+        public void changeTitle(object sender, EventArgs e)
+        {
+            Title = "My first web form";
+            //pageTitle.Text =Title ;
+        }
+
+        public void modifyPanelState(object sender, EventArgs e)
+        {
+            myPanel.Visible = !myPanel.Visible;
+        }
+
+        public void removeButton(object sender, EventArgs e)
+        {
+            if (Page.FindControl("dinamicButton") != null)
+                myPanel.Controls.Remove(Page.FindControl("dinamicButton"));
+        }
+
+        public void showItem(object sender, EventArgs e)
+        {
+            label.Text = list.SelectedValue;
+        }
+    }
+}
